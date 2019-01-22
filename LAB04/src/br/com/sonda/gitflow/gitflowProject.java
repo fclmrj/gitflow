c8e3c89223522b55f997462efe251e2d2376e0ac
Fabio Moura <fabio.moura@sonda.com>
2019-01-22 09:26:14 -0200

    Feature Updated Again

--- LAB04/src/br/com/sonda/gitflow/gitflowProject.java
+++ LAB04/src/br/com/sonda/gitflow/gitflowProject.java
@@ -5,5 +5,6 @@ public class gitflowProject {
 	public static void main(String[] args) {
 		System.out.println("Feature 1");
 		System.out.println("Feature Updated");
+		System.out.println("Feature Updated");
 	}
 }
